c9ed3c5ca9d0a86438c6c44bea010fdad1d57f43
GregShen <gregshen0925@gmail.com>
2022-07-11 21:06:58 +0800

    hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+import CeramicClient from '@ceramicnetwork/http-client'
+import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
+
+import { EthereumAuthProvider, ThreeIdConnect } from '@3id/connect'
+import { DID } from 'dids'
+import { IDX } from '@ceramicstudio/idx'
+
+const endpoint = "https://ceramic-clay.3boxlabs.com"
+
 
 function App() {
+  const [name, setName] = useState('')
+  const [image, setImage] = useState('')
+  const [loaded, setLoaded] = useState(false)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
