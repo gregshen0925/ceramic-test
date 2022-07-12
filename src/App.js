07d0f0fab4ebba0f7e36a650c96a711ae60e7e72
GregShen <gregshen0925@gmail.com>
2022-07-12 11:39:52 +0800

    done

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import { IDX } from '@ceramicstudio/idx'
 
 const endpoint = "https://ceramic-clay.3boxlabs.com"
 
-
 function App() {
   const [name, setName] = useState('')
   const [image, setImage] = useState('')
@@ -41,7 +40,7 @@ function App() {
       setLoaded(true)
     }
   }
-  
+
   async function updateProfile() {
     const [address] = await connect()
     const ceramic = new CeramicClient(endpoint)
@@ -69,10 +68,17 @@ function App() {
 
     console.log("Profile updated!")
   }
-  
+
   return (
     <div className="App">
+      <input placeholder="Name" onChange={e => setName(e.target.value)} />
+      <input placeholder="Profile Image" onChange={e => setImage(e.target.value)} />
+      <button onClick={updateProfile}>Set Profile</button>
+      <button onClick={readProfile}>Read Profile</button>
 
+      { name && <h3>{name}</h3> }
+      { image && <img style={{ width: '400px' }} src={image} /> }
+      {(!image && !name && loaded) && <h4>No profile, please create one...</h4>}
     </div>
   );
 }
