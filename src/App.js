fda3312332796b96969fed482bccdf77bfa88413
GregShen <gregshen0925@gmail.com>
2022-07-16 18:36:33 +0800

    add address read

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { EthereumAuthProvider, ThreeIdConnect } from '@3id/connect'
 import { DID } from 'dids'
 import { IDX } from '@ceramicstudio/idx'
 
+//this is free read/write node 
 const endpoint = "https://ceramic-clay.3boxlabs.com"
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
   const [sexOrientation, setSexOrientation] = useState('')
   const [randomNumber, setNumber] = useState('')
   const [loaded, setLoaded] = useState(false)
+  const [addressToRead, setAddressToRead] = useState('')
 
   async function connect() {
     const addresses = await window.ethereum.request({
@@ -27,6 +29,7 @@ function App() {
     return addresses
   }
 
+  // this is for reading connected address
   async function readProfile() {
     const [address] = await connect()
     const ceramic = new CeramicClient(endpoint)
@@ -36,6 +39,36 @@ function App() {
       const data = await idx.get(
         'basicProfile',
         `${address}@eip155:1`
+      )
+      console.log(`${address}@eip155:1`)
+      console.log('data: ', data)
+      if (data.name) setName(data.name)
+      if (data.description) setDescription(data.description)
+      if (data.avatar) setImage(data.avatar)
+      if (data.birthday) setBirthday(data.birthday)
+      if (data.gender) setGender(data.gender)
+      if (data.sexOrientation) setSexOrientation(data.sexOrientation)
+      if (data.randomNumber) setNumber(data.randomNumber)
+
+
+    } catch (error) {
+      console.log('error: ', error)
+      setLoaded(true)
+    }
+  }
+  //this is for reading other address's profile
+  async function readAddressProfile() {
+    const ceramic = new CeramicClient(endpoint)
+    const idx = new IDX({ ceramic })
+
+    if (!addressToRead) {
+      return alert("Input an address")
+    }
+
+    try {
+      const data = await idx.get(
+        'basicProfile',
+        `${addressToRead}@eip155:1`
       )
       console.log('data: ', data)
       if (data.name) setName(data.name)
@@ -98,15 +131,18 @@ function App() {
       <button onClick={updateProfile}>Set Profile</button>
       <button onClick={readProfile}>Read Profile</button>
       <button onClick={connect}>Connect Wallet</button>
+      <input placeholder="Address to read" onChange={e => setAddressToRead(e.target.value)} />
+      <button onClick={readAddressProfile}>Read Address Profile</button>
+
 
 
       {name && <h3>Name: {name}</h3>}
-      {name && <h3>Description: {description}</h3>}
+      {description && <h3>Description: {description}</h3>}
       {image && <img style={{ width: '400px' }} src={image} />}
-      {name && <h3>Birthday: {birthday}</h3>}
-      {name && <h3>Gender: {gender}</h3>}
-      {name && <h3>Sex Orientation: {sexOrientation}</h3>}
-      {name && <h3>Random Number: {randomNumber}</h3>}
+      {birthday && <h3>Birthday: {birthday}</h3>}
+      {gender && <h3>Gender: {gender}</h3>}
+      {sexOrientation && <h3>Sex Orientation: {sexOrientation}</h3>}
+      {randomNumber && <h3>Random Number: {randomNumber}</h3>}
       {(!image && !name && loaded) && <h4>No profile, please create one...</h4>}
     </div>
   );
